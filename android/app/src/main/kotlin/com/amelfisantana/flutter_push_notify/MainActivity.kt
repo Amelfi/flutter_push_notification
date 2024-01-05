52a48978be9bd94bbd621c859d9e6b97e314e099
Adan <adansantana@AGL-MacBook-Pro.local>
2024-01-05 11:09:25 -0400

    first commit

---
+++ android/app/src/main/kotlin/com/amelfisantana/flutter_push_notify/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.amelfisantana.flutter_push_notify
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/screens.dart
@@ -0,0 +1,4 @@
+export 'package:flutter_push_notify/screens/message_screen.dart';
+
+export 'package:flutter_push_notify/screens/home_screen.dart';
+
